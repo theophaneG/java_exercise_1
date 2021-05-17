7d2f2f894808efde92757369d77896b0dd77f4de
theophane Gros <theophane.gros@epita.fr>
2021-05-17 15:52:39 +0200

    java_exercice_1 Partie 2

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,14 +1,14 @@
-import javax.lang.model.type.NullType;
 import java.util.Scanner;
 
 public class Launcher {
     public static void main(String[] args) {
         System.out.println("Bienvenu en enfer!");
         String i = null;
-        do {
-            Scanner sc = new Scanner(System.in);
-            i = sc.next();
+        Scanner sc = new Scanner(System.in);
+        i = sc.next();
+        if ((!"quit".equals(i))) {
+        } else {
             System.out.println("unknow Command");
-        }while (!"quit".equals(i));
+        }
     }
 }
