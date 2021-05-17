b720f9329453abf2f02e3eaf49104e6f1b5886ec
theophane Gros <theophane.gros@epita.fr>
2021-05-17 16:37:22 +0200

    java_exercice_1 Partie 4 qvec check nombre

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,14 +1,39 @@
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Launcher {
     public static void main(String[] args) {
         System.out.println("Bienvenu en enfer!");
         String i = null;
-        Scanner sc = new Scanner(System.in);
-        i = sc.next();
-        if ((!"quit".equals(i))) {
-        } else {
-            System.out.println("unknow Command");
-        }
+        do {
+            Scanner sc = new Scanner(System.in);
+            i = sc.next();
+            if ("fibo".equals(i)) {
+                System.out.println("donner nombre");
+                int number = 0;
+                boolean isNumber = true;
+                while (isNumber) {
+                    try {
+                        number = sc.nextInt();
+
+                    } catch (NoSuchElementException e) {
+                        System.out.println("pas un nombre");
+                        continue;
+                    }finally {
+                        sc.nextLine();
+                    }
+                    isNumber = false;
+                }
+                System.out.println(i + ": " + fibo(number));
+                sc.nextLine();
+            } else {
+                System.out.println("unknow Command");
+            }
+        } while ((!"quit".equals(i)));
+    }
+
+    private static int fibo(int n) {
+        if (n <= 1) return n;
+        else return fibo(n - 1) + fibo(n - 2);
     }
 }
