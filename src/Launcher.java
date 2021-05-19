3c83de88bfa702758448dc9c317512f17b2297c0
theophane Gros <theophane.gros@epita.fr>
2021-05-19 15:25:08 +0200

    java_exercice_1 Partie 5 freq

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,12 +1,17 @@
-import java.util.NoSuchElementException;
-import java.util.Scanner;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Launcher {
     public static void main(String[] args) {
         System.out.println("Bienvenu en enfer!");
-        String i = null;
+        String i ;
+        Scanner sc = new Scanner(System.in);
         do {
-            Scanner sc = new Scanner(System.in);
             i = sc.next();
             if ("fibo".equals(i)) {
                 System.out.println("donner nombre");
@@ -19,13 +24,46 @@ public class Launcher {
                     } catch (NoSuchElementException e) {
                         System.out.println("pas un nombre");
                         continue;
-                    }finally {
+                    } finally {
                         sc.nextLine();
                     }
                     isNumber = false;
                 }
-                System.out.println(i + ": " + fibo(number));
+                System.out.println(fibo(number));
+            } else if ("freq".equals(i)) {
+                System.out.println("entrer chemin fichier");
+                i = sc.next();
                 sc.nextLine();
+                Path path =null;
+                try {
+                    path = Paths.get(i);
+                } catch (InvalidPathException e) {
+                    System.out.println("Unreadable File");
+                }
+                System.out.println(path);
+                try {
+                    i = java.nio.file.Files.readString(path);
+                }catch (java.io.IOException e){
+                    System.out.println(e);
+                }
+                String[] words = i.replaceAll("\\p{Punct}", " ").toLowerCase().split("\\s+");
+                Stream<String> wordstream = Arrays.stream(words);
+                Map<String, Long> countByword = wordstream
+                        .filter(s -> !s.isBlank())
+                        .map(s -> s.toLowerCase())
+                        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+                Comparator<Map.Entry<String, Long>> countreversed =
+                        Comparator.<Map.Entry<String, Long>, Long>comparing(e -> e.getValue())
+                                .reversed();
+
+                String reuslt = countByword.entrySet().stream()
+                        .sorted(countreversed)
+                        .limit(3)
+                        .map(e -> e.getKey())
+                        .collect(Collectors.joining(" "));
+                System.out.println(reuslt);
+
             } else {
                 System.out.println("unknow Command");
             }
